b620e35816b2b8ff5354e9d1ce4d1ab5346617af
Ckend <83493903@qq.com>
2017-05-10 13:40:26 +0800

    thx to leetcode

--- 400. Nth Digit/400. Nth Digit/main.cpp
+++ 400. Nth Digit/400. Nth Digit/main.cpp
@@ -9,7 +9,32 @@
 #include <iostream>
 class Solution{
 public:
-    int findNthDigit(int n){
+    int findNthDigit(long n){
+        
+        //求出这个位置的数字的位数
+        long howLong = 9, digits = 1;
+        while (n - howLong*digits > 0) {
+            n -= howLong*digits;
+            howLong = howLong * 10;
+            ++digits;
+        }
+        
+        //求出是什么数字
+        long index = n % digits;
+        if (index == 0)
+            index = digits;
+        long num = 1;
+        for (int i = 1; i < digits; ++i) {
+            num = num * 10;
+        }
+        //若为0则是上个数字的最后一位
+        num += (index == digits) ? n / digits - 1 : n / digits;
+        
+        //求出结果
+        for (long i = index; i < digits; ++i) {
+            num = num / 10;
+        }
+        return (int)num % 10;
         
     }
 };
